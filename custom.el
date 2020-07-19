3e12257d5e62959b2b11b33b4f8faaa05d389483
Nicky van Foreest <vanforeest@gmail.com>
2020-07-19 21:08:16 +0200

    moved to org mode

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/python/misc/pyeda/puzzle.org")))
  '(package-selected-packages
    (quote
-    (org magit csv-mode auctex blacken elpy deft flycheck company smex dimmer ack diminish material-theme use-package))))
+    (ox-gfm ox-hugo texmathp cdlatex org magit csv-mode auctex blacken elpy deft flycheck company smex dimmer ack diminish material-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

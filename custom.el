c08c9bb4b7a8c693f4a9cb16457c06b4eaaa060c
Nicky van Foreest <vanforeest@gmail.com>
2021-02-19 08:10:34 +0100

    ll

--- custom.el
+++ custom.el
@@ -8,8 +8,9 @@
     ("~/vakken/qts/queueing_book/simulations/6_simulations_event_stacks.org" "~/vakken/qsim/queueing_book/simulations/simultions_event_stacks.org" "~/python/misc/pyeda/puzzle.org")))
  '(package-selected-packages
    (quote
-    (ox-latex spaceline auto-package-update rainbow-delimiters htmlize magit csv-mode cdlatex auctex blacken elpy company flycheck atomic-chrome deft rg diminish ox-reveal ob-async org-bullets dimmer material-theme yasnippet-snippets yasnippet smex which-key bufler use-package)))
- '(safe-local-variable-values (quote ((TeX-master . t)))))
+    (latex tex-site no-littering ox-latex spaceline auto-package-update rainbow-delimiters htmlize magit csv-mode cdlatex auctex blacken elpy company flycheck atomic-chrome deft rg diminish ox-reveal ob-async org-bullets dimmer material-theme yasnippet-snippets yasnippet smex which-key bufler use-package)))
+ '(safe-local-variable-values (quote ((TeX-master . t))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

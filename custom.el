d23435bd5768273cb168e984a605df9f7f11d9c3
Nicky van Foreest <vanforeest@gmail.com>
2021-03-21 10:20:16 +0100

    fix

--- custom.el
+++ custom.el
@@ -3,14 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/vakken/qts/queueing_book/simulations/6_simulations_event_stacks.org" "~/vakken/qsim/queueing_book/simulations/simultions_event_stacks.org" "~/python/misc/pyeda/puzzle.org")))
  '(package-selected-packages
    (quote
-    (latex tex-site no-littering ox-latex spaceline auto-package-update rainbow-delimiters htmlize magit csv-mode cdlatex auctex blacken elpy company flycheck atomic-chrome deft rg diminish ox-reveal ob-async org-bullets dimmer material-theme yasnippet-snippets yasnippet smex which-key bufler use-package)))
- '(safe-local-variable-values (quote ((TeX-master . t))))
-)
+    (cdlatex auctex htmlize ob-async org-bullets blacken elpy flycheck deft rg diminish dimmer modus-themes yasnippet-snippets yasnippet dired-hide-dotfiles dired-single which-key bufler smex auto-package-update visual-fill-column no-littering use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
